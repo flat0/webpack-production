0525454d03dff5edb19cbe59420ae2910abb1277
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-26 07:06:03 +0600

    no message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// 2021-01-26
+import _ from 'lodash'; // 2021-01-26 https://webpack.js.org/guides/getting-started/#creating-a-bundle
 // 2021-01-26 https://webpack.js.org/guides/getting-started/#basic-setup
 function component() {
 	const element = document.createElement('div');
