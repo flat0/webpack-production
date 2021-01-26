533708fd085b9b811c7a688890f86979e8c54848
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-26 06:59:58 +0600

    no message

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// 2021-01-26 https://webpack.js.org/guides/getting-started/#basic-setup
+function component() {
+	const element = document.createElement('div');
+	// Lodash, currently included via a script, is required for this line to work
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	return element;
+}
+document.body.appendChild(component());
