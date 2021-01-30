ac84ed3d48cc5203180cff441610bae32520ff0c
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-30 10:48:10 +0600

    https://webpack.js.org/guides/asset-management/#loading-images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // 2021-01-26
 import _ from 'lodash'; // 2021-01-26 https://webpack.js.org/guides/getting-started/#creating-a-bundle
 import './style.css'; // 2021-01-30 https://webpack.js.org/guides/asset-management/#loading-cs
+import imageURL from './1.jpg'; // 2021-01-30 https://webpack.js.org/guides/asset-management/#loading-images
 // 2021-01-26 https://webpack.js.org/guides/getting-started/#basic-setup
 function component() {
 	const element = document.createElement('div');
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
+	const image = new Image;
+	image.src = imageURL;
+	element.appendChild(image);
 	return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
 			// 1) https://webpack.js.org/guides/asset-management/#loading-css
 			// 2) «Loaders are evaluated/executed from right to left»: https://webpack.js.org/concepts/loaders/#configuration
 			{test: /\.css$/i, use: ['style-loader', 'css-loader']}
+			// 2021-01-30 https://webpack.js.org/guides/asset-management/#loading-images
+			,{test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'}
 		]
 	}
 	,output: {filename: 'bundle.js', path: path.resolve(__dirname, 'dist')}
