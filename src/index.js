575b0a2a081f4a45260163a785fd17d89e7d4f37
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-30 10:22:11 +0600

    https://webpack.js.org/guides/asset-management/#loading-css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // 2021-01-26
 import _ from 'lodash'; // 2021-01-26 https://webpack.js.org/guides/getting-started/#creating-a-bundle
+import './style.css'; // 2021-01-30 https://webpack.js.org/guides/asset-management/#loading-cs
 // 2021-01-26 https://webpack.js.org/guides/getting-started/#basic-setup
 function component() {
 	const element = document.createElement('div');
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 	return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 // 2021-01-26 https://webpack.js.org/guides/getting-started/#using-a-configuration
 const path = require('path');
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+	entry: './src/index.js'
+	,module: {
+		rules: [
+			// 2021-01-30
+			// 1) https://webpack.js.org/guides/asset-management/#loading-css
+			// 2) «Loaders are evaluated/executed from right to left»: https://webpack.js.org/concepts/loaders/#configuration
+			{test: /\.css$/i, use: ['style-loader', 'css-loader']}
+		]
 	}
+	,output: {filename: 'bundle.js', path: path.resolve(__dirname, 'dist')}
 };
