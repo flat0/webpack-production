4f8d76317dae38b6a6e499a5bb55a9d5d87ee408
Dmitry Fedyuk <dfediuk@gmail.com>
2021-01-31 23:56:39 +0600

    no message

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@
 import './style.css'; // 2021-01-30 https://webpack.js.org/guides/asset-management/#loading-css
 import _ from 'lodash'; // 2021-01-26 https://webpack.js.org/guides/getting-started/#creating-a-bundle
 // 2021-01-31
-// 1) https://webpack.js.org/guides/asset-management/#loading-data
+// 1) «Instead of making an ajax request and parsing the data at runtime
+// you can load it into your module during the build process
+// so that the parsed data is ready to go as soon as the module hits the browser.»
+// https://webpack.js.org/guides/asset-management/#loading-data
 // 2) The «./data/.csv» path leads to the «Name expected» error.
 import csv from './data/1.csv';
 import imageURL from './1.jpg'; // 2021-01-30 https://webpack.js.org/guides/asset-management/#loading-images
 // 2021-01-31
-// 1) https://webpack.js.org/guides/asset-management/#loading-data
+// 1) «Instead of making an ajax request and parsing the data at runtime
+// you can load it into your module during the build process
+// so that the parsed data is ready to go as soon as the module hits the browser.»
+// https://webpack.js.org/guides/asset-management/#loading-data
 // 2) The «./data/.xml» path leads to the «Name expected» error.
 import xml from './data/1.xml';
 // 2021-01-26 https://webpack.js.org/guides/getting-started/#basic-setup
