f0fe58d0f82368293ced79d4cccbf643ec84c816
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-01 20:32:34 +0600

    https://webpack.js.org/guides/development/#using-watch-mode

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,14 @@ module.exports = {
 	,output: {filename: '[name].bundle.js', path: path.resolve(__dirname, 'dist')}
 	,plugins: [
 		// 2021-02-01 https://webpack.js.org/guides/output-management/#cleaning-up-the-dist-folder
-		new CleanWebpackPlugin()
+		new CleanWebpackPlugin({
+			// 2021-02-01
+			// «Tell CleanWebpackPlugin that we don't want to remove the index.html file
+			// after the incremental build triggered by `watch`.
+			// We do this with the cleanStaleWebpackAssets option.»:
+			// https://webpack.js.org/guides/development/#using-watch-mode
+			cleanStaleWebpackAssets: false
+		})
 		// 2021-02-01 https://webpack.js.org/guides/output-management/#setting-up-htmlwebpackplugin
 		,new HtmlWebpackPlugin({
 			title: 'Development' // 2021-02-01 https://webpack.js.org/guides/development
