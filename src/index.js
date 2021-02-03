8b3f324487a83c8835edd8d4f88d6d8a24f0142f
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-04 05:46:12 +0600

    https://webpack.js.org/guides/tree-shaking/#add-a-utility

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 // 2021-01-26
-import Print from './print'; // 2021-02-03 https://webpack.js.org/guides/caching#module-identifiers
+import {cube} from './math.js'; // 2021-02-04 https://webpack.js.org/guides/tree-shaking#add-a-utility
 // 2021-02-03 https://webpack.js.org/guides/code-splitting#dynamic-imports
-function getComponent() {return import('lodash')
-	// 2021-02-03
-	// «The reason we need `default`
-	// is that since webpack 4, when importing a CommonJS module,
-	// the import will no longer resolve to the value of module.exports,
-	// it will instead create an artificial namespace object for the CommonJS module.»
-	// https://webpack.js.org/guides/code-splitting#dynamic-imports
-	// https://medium.com/webpack/webpack-4-import-and-commonjs-d619d626b655
-	.then(({default: _}) => {
-		const e = document.createElement('div');
-		e.innerHTML = _.join(['Hello', 'webpack'], ' ');
-		e.onclick = Print.bind(null, 'Hello webpack!');
-		return e;
-	})
-	.catch((error) => 'An error occurred while loading the component')
-;}
-getComponent().then(с => document.body.appendChild(с));
+function component() {
+	const element = document.createElement('pre');
+	element.innerHTML = [
+		'Hello webpack!',
+		'5 cubed is equal to ' + cube(5)
+	].join('\n\n');
+	return element;
+}
+document.body.appendChild(component());
--- src/print.js
+++
@@ -1,2 +0,0 @@
-// 2021-02-03 https://webpack.js.org/guides/caching#module-identifiers
-export default function print(s) {console.log(s);};
