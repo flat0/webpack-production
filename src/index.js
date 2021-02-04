1be9895f7dba20903120fb8118cb3f5c8d6cbda1
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-04 07:57:58 +0600

    https://webpack.js.org/guides/production#specify-the-mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // 2021-01-26
 import {cube} from './math.js'; // 2021-02-04 https://webpack.js.org/guides/tree-shaking#add-a-utility
+// 2021-02-04 https://webpack.js.org/guides/production#specify-the-mode
+if ('production' !== process.env.NODE_ENV) {
+	console.log('Looks like we are in development mode!');
+}
 // 2021-02-03 https://webpack.js.org/guides/code-splitting#dynamic-imports
 function component() {
 	const element = document.createElement('pre');
