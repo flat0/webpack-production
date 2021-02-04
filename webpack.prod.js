d24bfc348ecfedf53bb63c394b6e7c5bc96e8e9e
Dmitry Fedyuk <dfediuk@gmail.com>
2021-02-04 07:58:49 +0600

    https://webpack.js.org/guides/production/#source-mapping

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,7 @@
 // 2021-02-04 https://webpack.js.org/guides/production#setup
 const common = require('./webpack.common.js');
 const {merge} = require('webpack-merge');
-module.exports = merge(common, {mode: 'production'});
+module.exports = merge(common, {
+	devtool: 'source-map' // 2021-02-04 https://webpack.js.org/guides/production#source-mapping
+	,mode: 'production'
+});
